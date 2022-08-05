5301135fcb8dd7d544368d587093c1fb3ab444c6
Veaceslav Chirita <veaceslav.chirita@devexperts.com>
2022-08-05 19:43:23 +0300

    Initial Commit

---
+++ ItunesSearchApp/ItunesSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItunesSearchAppApp.swift
+//  ItunesSearchApp
+//
+//  Created by Veaceslav Chirita on 05.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ItunesSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
